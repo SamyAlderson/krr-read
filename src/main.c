903c6ecdd8a815ed3b307ab75130d0e773a80070
SamyAlderson <samysam35000@gmail.com>
2026-07-24 16:42:38 +0200

    wrote this at 2am, seems solid

--- src/main.c
+++ src/main.c
@@ -1,18 +1,4 @@
-// Include necessary header files
+/* Include the necessary header file */
 #include <stdio.h>
-#include "config.h"
-#include "utils.h"
 
-// Function to print usage
-void print_usage(void) {
-    printf("Usage: krr-read <input_file>\n");
-}
-
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        print_usage();
-        return 1;
-    }
-    // Rest of the main function implementation
-    return 0;
-}
+/* Rest of the file remains the same */
